a3fe75a27cffb790e4e01291e308ba16136e01cb
Richard Stanley <audstanley@gmail.com>
2020-09-11 09:43:02 -0700

    moving the production folder content to docs/

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const STORAGE = path.resolve(__dirname, '__storage__')
 const EXCLUDE_DEFAULT = [NODE_MODULES, EXTERNALS, STORAGE]
 
 const SRC = path.resolve(__dirname, 'src')
-const DIST = path.resolve(__dirname, 'dist')
+const DIST = path.resolve(__dirname, 'docs')
 
 const NODE_ENV = process.env.NODE_ENV
 const MODE = NODE_ENV !== 'development' ? 'production' : 'development'
