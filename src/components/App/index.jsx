0b96c4b0e4c8d5e17059b4b4ef84e67c2e626855
Richard Stanley <audstanley@gmail.com>
2020-10-17 11:39:33 -0700

    sorting functions ready to go for finishing

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
-
+import React from 'react';
+import { hot } from 'react-hot-loader';
 import AppContent from '../AppContent'
-
-import AppStateProvider from './AppStateProvider'
-
-import './style.css'
+import AppStateProvider from './AppStateProvider';
+import './style.css';
 
 function App() {
     return (
